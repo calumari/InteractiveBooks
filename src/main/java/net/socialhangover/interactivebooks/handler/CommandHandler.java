cf808a476ea6a1abb1a79b43988dba3ddca3c1a5
Callum <38501234+calumari@users.noreply.github.com>
2020-06-28 11:26:39 +0100

    Add create template command

--- src/main/java/net/socialhangover/interactivebooks/handler/CommandHandler.java
+++ src/main/java/net/socialhangover/interactivebooks/handler/CommandHandler.java
@@ -17,24 +17,51 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommandHandler implements TerminableModule {
 
-    private static final List<String> completions = ImmutableList.of("open", "give", "reload");
+    private static final List<String> completions = ImmutableList.of("open", "create", "give", "reload");
 
     private final InteractiveBooksPlugin plugin;
 
     @Override
     public void setup(@Nonnull TerminableConsumer consumer) {
         Commands.create()
-                .tabHandler(c -> TabCompleter.create()
-                        .at(0, CompletionSupplier.startsWith(getFilteredTabComplete(c.sender())))
-                        .complete(c.args()))
+                .tabHandler(c -> {
+                    TabCompleter completer = TabCompleter.create()
+                            .at(0, CompletionSupplier.startsWith(getFilteredTabComplete(c.sender())));
+
+                    String root = c.arg(0).parse(String.class).orElse(null);
+                    if (root == null) {
+                        return completer.complete(c.args());
+                    }
+
+                    if (root.equalsIgnoreCase("create")) {
+                        return completer.at(1, CompletionSupplier.startsWith("areallycoolbook", "ataleofadeveloper"))
+                                .complete(c.args());
+                    }
+                    return completer.complete(c.args());
+                })
                 .handler(c -> {
                     String root = c.arg(0).parse(String.class).orElse(null);
                     if (root == null) {
                         return;
                     }
-                    switch (root.toLowerCase()) {
-                        case "reload":
-                            plugin.reload();
+
+                    if (root.equalsIgnoreCase("create")) {
+                        String name = c.arg(1).parse(String.class).orElse(null);
+                        if (name == null) {
+                            c.reply("missing filename"); // todo locale
+                            return;
+                        }
+                        if (plugin.createTemplate(name) != null) {
+                            c.reply("file created. use /ibooks reload to load"); // todo locale
+                        } else {
+                            c.reply("file name exists?"); // todo locale
+                        }
+                        return;
+                    }
+
+                    if (root.equalsIgnoreCase("reload")) {
+                        plugin.reload();
+                        c.reply("plugin reloaded :)");
                     }
                 })
                 .registerAndBind(consumer, "ibooks", "interactivebooks", "ib");
