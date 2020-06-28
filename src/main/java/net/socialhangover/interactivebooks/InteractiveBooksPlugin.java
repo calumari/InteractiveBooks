28e8bf859c6dbc1cf8b1a12c03e103d45a4dc73e
Callum <38501234+calumari@users.noreply.github.com>
2020-06-28 11:17:25 +0100

    Fix book loading

--- src/main/java/net/socialhangover/interactivebooks/InteractiveBooksPlugin.java
+++ src/main/java/net/socialhangover/interactivebooks/InteractiveBooksPlugin.java
@@ -2,6 +2,7 @@ package net.socialhangover.interactivebooks;
 
 import com.google.common.collect.ImmutableList;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import me.lucko.helper.plugin.ExtendedJavaPlugin;
 import net.socialhangover.interactivebooks.handler.CommandHandler;
 import net.socialhangover.interactivebooks.handler.PlayerListener;
@@ -9,6 +10,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -60,19 +62,24 @@ public final class InteractiveBooksPlugin extends ExtendedJavaPlugin {
         books.remove(id);
     }
 
+    @SneakyThrows
     private void loadAll() {
         config = loadConfig("config.yml");
-//        File folder = getRelativeFile("books");
-        File example = new File(getDataFolder(), "examplebook.yml");
-        if (!example.exists() && !config.getBoolean("save-example", false)) {
-            saveResource("examplebook.yml", false);
+
+        File folder = getRelativeFile("books");
+        folder.mkdirs();
+
+        if (config.getBoolean("load-example", true)) {
+            File example = new File(folder, "examplebook.yml");
+            if (!example.exists()) {
+                Files.copy(getResource("examplebook.yml"), example.toPath());
+            }
+            registerBook(new IBook("example", YamlConfiguration.loadConfiguration(example)));
         }
-        registerBook(new IBook("example", YamlConfiguration.loadConfiguration(example)));
-//        loadBookConfigs(folder);
+        loadBookConfigs(folder);
     }
 
     private void loadBookConfigs(File folder) {
-        folder.mkdirs();
         for (File file : Objects.requireNonNull(folder.listFiles())) {
             if (file.getName().endsWith(".yml")) {
                 registerBook(new IBook(file.getName().substring(0, file.getName().length() - 4), YamlConfiguration.loadConfiguration(file)));
