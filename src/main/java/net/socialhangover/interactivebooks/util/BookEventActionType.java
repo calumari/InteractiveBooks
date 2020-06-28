6eb269052531ce07326bbf4cbd952c258dd70366
Callum <38501234+calumari@users.noreply.github.com>
2020-06-28 09:26:39 +0100

    Cleanup

--- src/main/java/net/socialhangover/interactivebooks/util/BookEventActionType.java
+++ src/main/java/net/socialhangover/interactivebooks/util/BookEventActionType.java
@@ -1,7 +1,10 @@
 package net.socialhangover.interactivebooks.util;
 
-enum BookEventActionType {
+import lombok.Getter;
 
+import javax.annotation.Nullable;
+
+enum BookEventActionType {
     SHOW_TEXT("show text", "tooltip"),
     SHOW_ITEM("show item", "item"),
     SHOW_ENTITY("show entity", "entity"),
@@ -12,22 +15,22 @@ enum BookEventActionType {
     CHANGE_PAGE("change page"),
     COPY_TO_CLIPBOARD("copy");
 
+    @Nullable
     static BookEventActionType parse(String type) {
-        for (BookEventActionType actionType : values())
-            for (String identifier : actionType.getAliases())
-                if (type.equalsIgnoreCase(identifier))
+        for (BookEventActionType actionType : values()) {
+            for (String identifier : actionType.getAliases()) {
+                if (type.equalsIgnoreCase(identifier)) {
                     return actionType;
+                }
+            }
+        }
         return null;
     }
 
+    @Getter
     private final String[] aliases;
 
     BookEventActionType(String... aliases) {
         this.aliases = aliases;
     }
-
-    public String[] getAliases() {
-        return aliases;
-    }
-
 }
