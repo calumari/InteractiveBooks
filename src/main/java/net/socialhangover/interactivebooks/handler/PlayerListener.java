2e41be0d4b11f30f8a93a1cd3e97f5172c2cdaeb
Callum <38501234+calumari@users.noreply.github.com>
2020-06-28 13:54:36 +0100

    Update event listeners

--- src/main/java/net/socialhangover/interactivebooks/handler/PlayerListener.java
+++ src/main/java/net/socialhangover/interactivebooks/handler/PlayerListener.java
@@ -1,17 +1,20 @@
 package net.socialhangover.interactivebooks.handler;
 
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import lombok.RequiredArgsConstructor;
 import me.lucko.helper.Events;
-import me.lucko.helper.reflect.MinecraftVersion;
-import me.lucko.helper.reflect.MinecraftVersions;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
 import net.socialhangover.interactivebooks.IBook;
 import net.socialhangover.interactivebooks.InteractiveBooksPlugin;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -19,62 +22,61 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlayerListener implements TerminableModule {
 
-    private static final boolean MC_AFTER_1_14 = MinecraftVersion.getRuntimeVersion().isAfterOrEq(MinecraftVersions.v1_14);
-
     private final InteractiveBooksPlugin plugin;
 
     @Override
     public void setup(@Nonnull TerminableConsumer consumer) {
         Events.subscribe(PlayerJoinEvent.class)
                 .handler(e -> {
+                    Player player = e.getPlayer();
+
                     String openBookId;
                     List<String> booksToGiveIds;
-                    if (e.getPlayer().hasPlayedBefore()) {
+                    if (player.hasPlayedBefore()) {
                         openBookId = plugin.getConfig().getString("open_book_on_join");
                         booksToGiveIds = plugin.getConfig().getStringList("books_on_join");
                     } else {
                         openBookId = plugin.getConfig().getString("open_book_on_first_join");
                         booksToGiveIds = plugin.getConfig().getStringList("books_on_first_join");
                     }
-                    if (openBookId != null && plugin.getBook(openBookId) != null && e.getPlayer().hasPermission("interactivebooks.open." + openBookId)) {
+
+                    if (openBookId != null && plugin.getBook(openBookId) != null && player.hasPermission("interactivebooks.open." + openBookId)) {
                         IBook book = plugin.getBook(openBookId);
                         if (book != null) {
-                            if (MC_AFTER_1_14)
-                                book.open(e.getPlayer());
-                            else
-                                Bukkit.getScheduler().runTask(plugin, () -> book.open(e.getPlayer()));
+                            book.open(e.getPlayer());
                         }
                     }
 
                     booksToGiveIds.forEach(id -> {
                         IBook book = plugin.getBook(id);
-                        if (book != null)
-                            e.getPlayer().getInventory().addItem(book.getItem(e.getPlayer()));
+                        if (book != null) {
+                            player.getInventory().addItem(book.getTaggedItem(player));
+                        }
                     });
                 })
                 .bindWith(consumer);
 
-//        Events.subscribe(PlayerInteractEvent.class)
-//                .handler(e -> {
-//                    if (e.useItemInHand().equals(Event.Result.DENY))
-//                        return;
-//                    if (!e.getAction().equals(Action.RIGHT_CLICK_AIR) && !e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-//                        return;
-//                    if (!plugin.getConfig().getBoolean("update_books_on_use"))
-//                        return;
-//                    if (!BooksUtils.getItemInMainHand(e.getPlayer()).getType().equals(Material.WRITTEN_BOOK))
-//                        return;
-//                    NBTItem nbti = new NBTItem(BooksUtils.getItemInMainHand(e.getPlayer()));
-//                    if (!nbti.hasKey("InteractiveBooks|Book-Id"))
-//                        return;
-//                    IBook book = plugin.getBook(nbti.getString("InteractiveBooks|Book-Id"));
-//                    if (book == null)
-//                        return;
-//                    ItemStack bookItem = book.getItem(e.getPlayer());
-//                    bookItem.setAmount(BooksUtils.getItemInMainHand(e.getPlayer()).getAmount());
-//                    BooksUtils.setItemInMainHand(e.getPlayer(), bookItem);
-//                })
-//                .bindWith(consumer);
+        Events.subscribe(PlayerInteractEvent.class)
+                .filter(e -> e.useItemInHand() != Event.Result.DENY && e.getHand() != null)
+                .filter(e -> e.hasItem() && e.getItem().getType() == Material.WRITTEN_BOOK)
+                .filter(e -> e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)
+                .filter(e -> plugin.getConfig().getBoolean("update_books_on_use"))
+                .handler(e -> {
+                    ItemStack item = e.getItem();
+                    NBTItem nbti = new NBTItem(item);
+                    if (!nbti.hasKey(IBook.BOOK_ID_KEY)) {
+                        return;
+                    }
+                    IBook book = plugin.getBook(nbti.getString(IBook.BOOK_ID_KEY));
+                    if (book == null) {
+                        return;
+                    }
+                    Player player = e.getPlayer();
+                    ItemStack bookItem = book.getTaggedItem(player);
+                    bookItem.setAmount(item.getAmount());
+                    player.getInventory().setItem(e.getHand(), bookItem);
+                })
+                .bindWith(consumer);
 
         Events.subscribe(PlayerCommandPreprocessEvent.class)
                 .handler(e -> {
